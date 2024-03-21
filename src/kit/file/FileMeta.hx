18751a153a87722cc0045a8f98a57dcc0d954283
wartman <faxmaster@gmail.com>
2024-03-21 11:11:27 -0500

    Add fullPath to FileMeta

--- src/kit/file/FileMeta.hx
+++ src/kit/file/FileMeta.hx
@@ -2,6 +2,7 @@ package kit.file;
 
 typedef FileMeta = {
 	public var path:String;
+	public var fullPath:String;
 	public var name:String;
 	public var created:Date;
 	public var updated:Date;
--- src/kit/file/adaptor/SysAdaptor.hx
+++ src/kit/file/adaptor/SysAdaptor.hx
@@ -24,6 +24,7 @@ class SysAdaptor implements Adaptor {
 		return Task.resolve({
 			path: path,
 			name: path.withoutDirectory().withoutExtension(),
+			fullPath: fullPath,
 			created: info.ctime,
 			updated: info.mtime,
 			size: info.size
